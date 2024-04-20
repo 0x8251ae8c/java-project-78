d2817ffa3695745f00255c105a4faeb09d9e7fd3
0x8251ae8c <99046156+0x8251ae8c@users.noreply.github.com>
2024-04-21 02:14:19 +0700

    Add class MapSchema

--- app/src/main/java/hexlet/code/Validator.java
+++ app/src/main/java/hexlet/code/Validator.java
@@ -3,10 +3,12 @@ package hexlet.code;
 public class Validator {
     private final StringSchema stringSchema;
     private final NumberSchema numberSchema;
+    private final MapSchema mapSchema;
 
     public Validator() {
         this.stringSchema = new StringSchema();
         this.numberSchema = new NumberSchema();
+        this.mapSchema = new MapSchema();
     }
 
     public StringSchema string() {
@@ -15,5 +17,9 @@ public class Validator {
 
     public NumberSchema number() {
         return numberSchema;
+    }
+
+    public MapSchema map() {
+        return mapSchema;
     }
 }
---
+++ app/src/test/java/hexlet/code/MapSchemaTest.java
@@ -0,0 +1,35 @@
+package hexlet.code;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MapSchemaTest {
+    @Test
+    public void testNumberSchema() {
+        var schema = new MapSchema();
+
+        assertTrue(schema.isValid(null));
+
+        schema.required();
+
+        assertFalse(schema.isValid(null));
+        assertTrue(schema.isValid(new HashMap<>()));
+
+        var data = new HashMap<String, String>();
+        data.put("key1", "value1");
+
+        assertTrue(schema.isValid(data));
+
+        schema.sizeof(2);
+
+        assertFalse(schema.isValid(data));
+
+        data.put("key2", "value2");
+
+        assertTrue(schema.isValid(data));
+    }
+}

bd55ed1a5f5f5ac31d5610d68cbf87f583125046
0x8251ae8c <99046156+0x8251ae8c@users.noreply.github.com>
2024-12-22 00:26:58 +0700

    Create gradle project

---
+++ app/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    id("java")
+    application
+    checkstyle
+    jacoco
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+application {
+    mainClass = "hexlet.code.App"
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.3"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.required.set(true)
+    }
+}
